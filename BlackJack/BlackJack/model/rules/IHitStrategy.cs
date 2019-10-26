231efc21e2146eba1c9742f73e4647b096969317
Lone Nilsson <onlylonely1986@gmail.com>
2019-10-26 13:23:26 +0200

    it works

--- BlackJack/BlackJack/model/rules/IHitStrategy.cs
+++ BlackJack/BlackJack/model/rules/IHitStrategy.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlackJack.model.rules
 {
-    class IHitStrategy
+    interface IHitStrategy
     {
+        bool DoHit(model.Player a_dealer);
     }
 }
--- BlackJack/BlackJack/model/rules/RulesFactory.cs
+++ BlackJack/BlackJack/model/rules/RulesFactory.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlackJack.model.rules
 {
     class RulesFactory
     {
+        // TODO här väljs det vilka regler som spelas med
+        public IHitStrategy GetHitRule()
+        {
+            // return new BasicHitStrategy();
+            return new Soft17HitStrategy();
+        }
+
+        public INewGameStrategy GetNewGameRule()
+        {
+            return new InternationalNewGameStrategy();
+            // return new AmericanNewGameStrategy();
+        }
     }
 }
